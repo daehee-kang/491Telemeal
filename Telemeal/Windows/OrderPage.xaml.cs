2bc063c4a9c0736879c4bc79ed677c005b6794a2
daehee-kang <daehee6316@gmail.com>
2018-01-29 21:59:16 -0800

    50% done for menu w/ img

--- Telemeal/Windows/OrderPage.xaml.cs
+++ Telemeal/Windows/OrderPage.xaml.cs
@@ -18,7 +18,7 @@ namespace Telemeal.Windows
     public partial class OrderPage : Window
     { 
         List<Food> foods = new List<Food>();
-        List<Button> buttons = new List<Button>();
+        List<Grid> grids = new List<Grid>();
         double tax = 0.1;
         double total = 0;
 
@@ -30,50 +30,77 @@ namespace Telemeal.Windows
             this.taxTBox.Text = string.Format("{0:F2}", total * tax);
             this.subtotalTBox.Text = string.Format("{0:F2}", (total + Double.Parse(taxTBox.Text)));
 
-            foods.Add(new Food() { FoodID = 1, Name = "Hamburger", Price = 2.50, Img = "/Telemeal;component/hamburger.png", FdCtgr = Food_Category.Main });
-            foods.Add(new Food() { FoodID = 2, Name = "Cheeseburger", Price = 3.00, Img = "", FdCtgr = Food_Category.Main });
-            foods.Add(new Food() { FoodID = 3, Name = "Double Double Burger", Price = 4.00, Img = "", FdCtgr = Food_Category.Main });
-            foods.Add(new Food() { FoodID = 4, Name = "French Fries", Price = 1.50, Img = "", FdCtgr = Food_Category.Appetizer });
-            foods.Add(new Food() { FoodID = 5, Name = "Animal Fries", Price = 3.00, Img = "", FdCtgr = Food_Category.Appetizer });
+            foods.Add(new Food() { FoodID = 1, Name = "Hamburger", Price = 2.50, Img = "/Telemeal;component/Images/hamburger.png", FdCtgr = Food_Category.Main });
+            foods.Add(new Food() { FoodID = 2, Name = "Cheeseburger", Price = 3.00, Img = "/Telemeal;component/Images/cheeseburger.png", FdCtgr = Food_Category.Main });
+            foods.Add(new Food() { FoodID = 3, Name = "Double Double Burger", Price = 4.00, Img = "/Telemeal;component/Images/doubleburger.png", FdCtgr = Food_Category.Main });
+            foods.Add(new Food() { FoodID = 4, Name = "French Fries", Price = 1.50, Img = "/Telemeal;component/Images/fries.jpg", FdCtgr = Food_Category.Appetizer });
+            foods.Add(new Food() { FoodID = 5, Name = "Animal Fries", Price = 3.00, Img = "/Telemeal;component/Images/animal.jpg", FdCtgr = Food_Category.Appetizer });
             foods.Add(new Food() { FoodID = 6, Name = "sm Drink", Price = 1.50, Img = "", FdCtgr = Food_Category.Drink });
             foods.Add(new Food() { FoodID = 7, Name = "lg Drink", Price = 2.00, Img = "", FdCtgr = Food_Category.Drink });
-            foods.Add(new Food() { FoodID = 8, Name = "Milkshake", Price = 3.00, Img = "", FdCtgr = Food_Category.Dessert });
+            foods.Add(new Food() { FoodID = 8, Name = "Milkshake", Price = 3.00, Img = "/Telemeal;component/Images/milkshake.jpg", FdCtgr = Food_Category.Dessert });
             foods.Add(new Food() { FoodID = 9, Name = "Cookie", Price = 1.00, Img = "", FdCtgr = Food_Category.Dessert });
 
             foreach(Food f in foods)
             {
-                Button b = new Button();
-                WrapPanel wp = new WrapPanel();
-                /*
+                Grid grid = new Grid();
+                grid.Height = 100;
+                grid.ShowGridLines = true;
+                grid.Background = new SolidColorBrush(Colors.AntiqueWhite);
+
+                ColumnDefinition gridCol1 = new ColumnDefinition();
+                ColumnDefinition gridCol2 = new ColumnDefinition();
+                gridCol1.Width = new GridLength(3, GridUnitType.Star);
+                gridCol2.Width = new GridLength(8, GridUnitType.Star);
+                grid.ColumnDefinitions.Add(gridCol1);
+                grid.ColumnDefinitions.Add(gridCol2);
+
                 Image image = new Image
                 {
-                    Source = new BitmapImage(new Uri(f.Img))
+                    Source = new BitmapImage(new Uri(f.Img, UriKind.Relative))
                 };
-                wp.Children.Add(image);
-                */
+                image.Stretch = Stretch.Fill;
+                Grid.SetColumn(image, 0);
+                grid.Children.Add(image);
+
+                DockPanel dp = new DockPanel();
+
+                TextBlock price = new TextBlock
+                {
+                    Text = f.Price.ToString(),
+                    Width = 30
+                };
+                DockPanel.SetDock(price, Dock.Right);
+                dp.Children.Add(price);
+
                 TextBlock name = new TextBlock
                 {
                     Text = f.Name
                 };
-                wp.Children.Add(name);
+                name.TextAlignment = TextAlignment.Left;
+                DockPanel.SetDock(name, Dock.Top);
+                dp.Children.Add(name);
+
+                TextBlock category = new TextBlock
+                {
+                    Text = f.FdCtgr.ToString()
+                };
+                category.TextAlignment = TextAlignment.Left;
+                DockPanel.SetDock(category, Dock.Top);
+                dp.Children.Add(category);
 
                 TextBlock desc = new TextBlock
                 {
                     Text = f.Description
                 };
-                wp.Children.Add(desc);
+                desc.TextAlignment = TextAlignment.Left;
+                dp.Children.Add(desc);
 
-                TextBlock price = new TextBlock
-                {
-                    Text = f.Price.ToString()
-                };
-                wp.Children.Add(price);
+                Grid.SetColumn(dp, 1);
+                grid.Children.Add(dp);
 
-                b.Content = wp;
+                grids.Add(grid);
 
-                buttons.Add(b);
-
-                Menu.Children.Add(b);
+                Menu.Children.Add(grid);
             }
 
             /*
